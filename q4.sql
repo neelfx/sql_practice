7c0da8c8878eec3b8b0f3f2056247188b006e3f8
neelfx <44417142+neelfx@users.noreply.github.com>
2021-04-18 20:16:11 +0100

    Add files via upload

---
+++ q4.sql
@@ -0,0 +1,8 @@
+/*
+ Write an SQL query to print the name of employees having the highest salary in each department.
+*/
+ 
+ 
+SELECT t.DEPARTMENT,t.FIRST_NAME,t.Salary from(SELECT max(Salary) as TotalSalary,DEPARTMENT from Worker group by DEPARTMENT) as TempNew 
+Inner Join Worker t on TempNew.DEPARTMENT=t.DEPARTMENT 
+ and TempNew.TotalSalary=t.Salary;
