37c8361d4e928e6da81ea3e36e243157d866cb6a
neelfx <44417142+neelfx@users.noreply.github.com>
2021-04-25 22:19:23 +0100

    Add files via upload

--- sql_tests_application.sql
+++ sql_tests_application.sql
@@ -16,3 +16,23 @@ select created_at as date, count(id) as number_created
 from hiring.customers
 group by created_at
 limit 1;
+
+
+/*
+Question 3: Where do most of the customers sign up from?
+Answer: Leeds
+*/
+
+select cities.name as city, count(customers.id) as customer_number
+from hiring.cities as cities, hiring.customers as customers
+where cities.id = customers.city_id
+group by city
+order by customer_number desc;
+
+-- OR
+
+select cities.name as city, count(customers.id) as customer_number
+from hiring.cities as cities
+left join hiring.customers as customers on cities.id = customers.city_id
+group by city
+order by customer_number desc;
